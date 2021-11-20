2b15f6f69e40087c678708c964b900856e97c2ac
dragan1810 <d.novakovic1810@yahoo.com>
2021-11-20 22:13:03 +0100

    qqq

--- Inventory-service/Utils/HealthzCheck.cs
+++ Inventory-service/Utils/HealthzCheck.cs
@@ -1,23 +1,44 @@
 using Microsoft.AspNetCore.Mvc;
-using InventoryService.Models;
-using InventoryService.Interfaces;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
 
 namespace InventoryService.Utils
 {
     [Route("/healthz")]
     [ApiController]
-    public class HealthController : Controller
+    public class HealthController : IHealthCheck
     {
         public HealthController()
         {
         }
+
+        public Task<HealthCheckResult> CheckHealthAsync(
+       HealthCheckContext context,
+       CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var healthCheckResultHealthy = true;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("A healthy result."));
+            }
+
+            return Task.FromResult(
+                new HealthCheckResult(context.Registration.FailureStatus,
+                "An unhealthy result."));
+        }
+
 
         [HttpGet]
         public String HealthCheck()
         {
             return "Hello";
         }
+
     }
 }
 

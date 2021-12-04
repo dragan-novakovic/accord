20c8b5e9d3c337deff0c58c2ceae064107d7203f
dragan1810 <d.novakovic1810@yahoo.com>
2021-12-04 20:49:01 +0100

    qqq

--- room-service/src/main.ts
+++ room-service/src/main.ts
@@ -1,8 +1,22 @@
+import { join } from "path";
+import {
+  Transport,
+  ClientOptions,
+  MicroserviceOptions,
+} from "@nestjs/microservices";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: ["user"],
+      protoPath: [join(__dirname, "./user/user.proto")],
+    },
+  });
+
   await app.listen(3003);
 }
 bootstrap();
--- room-service/src/rooms/rooms.entity.ts
+++ room-service/src/rooms/rooms.entity.ts
@@ -10,6 +10,6 @@ export class RoomEntity {
   @Column()
   name: string;
 
-  @OneToMany(() => ChannelEntity, (channel) => channel.room, { eager: true })
+  @OneToMany(() => ChannelEntity, (channel) => channel.room)
   channels: ChannelEntity[];
 }

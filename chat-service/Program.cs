98075246b9af8039c34b74522dbd19dde3d92da4
dragan1810 <d.novakovic1810@yahoo.com>
2021-12-05 01:39:33 +0100

    qq

--- chat-service/Program.cs
+++ chat-service/Program.cs
@@ -1,8 +1,12 @@
+using SignalRChat.Hubs;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSignalR();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();

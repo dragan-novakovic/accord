3cbdfeacf099ad69c1949666f4cc442ba32a28c2
dragan1810 <d.novakovic1810@yahoo.com>
2022-09-04 15:56:02 +0200

    qqq

--- room-service/data/seed/index.ts
+++ room-service/data/seed/index.ts
@@ -6,6 +6,36 @@ import { query } from "../../src/dbAccess/index";
 
 export default function seeder() {}
 
-function migrations() {
-  query(``, []);
+async function migrations() {
+  // create all needed tables
+  await query(
+    `CREATE TABLE IF NOT EXISTS rooms (
+  id varchar(45) NOT NULL,
+  name varchar(45) NOT NULL,
+  enabled integer NOT NULL DEFAULT '1',
+  PRIMARY KEY (id)
+)`,
+    []
+  );
+  // create users_rooms
+  await query(
+    `CREATE TABLE IF NOT EXISTS users_rooms (
+  user_id varchar(45) REFERENCES users (id),
+  room_id varchar(45) REFERENCES rooms (id)
+)`,
+    []
+  );
+  // create channels
+  await query(
+    `CREATE TABLE IF NOT EXISTS channels (
+  id varchar(45) NOT NULL,
+  name varchar(45) NOT NULL,
+  room_id varchar(45) REFERENCES rooms (id),
+  enabled integer NOT NULL DEFAULT '1',
+  PRIMARY KEY (id)
+)`,
+    []
+  );
 }
+
+migrations().then((x) => console.log(x));

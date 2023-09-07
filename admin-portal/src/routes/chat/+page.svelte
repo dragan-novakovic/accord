7812d79119910b1d66c1796a48f9cf75e59b0fd7
Dragan <d.novakovic1810@yahoo.com>
2023-09-07 09:52:55 +0200

    fix admin-portal docker dev setup

--- admin-portal/src/routes/chat/+page.svelte
+++ admin-portal/src/routes/chat/+page.svelte
@@ -3,42 +3,38 @@
 </script>
 
 <script lang="ts">
-import {HubConnectionBuilder, HttpTransportType} from '@microsoft/signalr';
-import { onMount } from 'svelte';
+	import { HubConnectionBuilder, HttpTransportType } from '@microsoft/signalr';
+	import { onMount } from 'svelte';
 
+	let connection = new HubConnectionBuilder()
+		.withUrl('http://localhost:5207/chat', {
+			withCredentials: false,
+			skipNegotiation: true,
+			transport: HttpTransportType.WebSockets
+		})
+		.build();
 
-let connection = new HubConnectionBuilder()
-    .withUrl("http://localhost:5207/chat", {withCredentials: false, skipNegotiation: true, transport: HttpTransportType.WebSockets})
-    .build();
+	// Prebaci u state-managment
+	let serverData = null;
+	let msg = null;
 
-// Prebaci u state-managment
-let serverData = null;
-let msg = null;
+	onMount(() => {
+		connection.start().catch((e) => console.log('On Mount Err', e));
+	});
 
+	connection.on('send', (data) => {
+		console.log('Zasto zoves send?', data);
+	});
+	connection.on('receive', (data) => {
+		console.log('Podaci sa servera', data);
+		serverData = serverData ? serverData + data : data;
+	});
 
+	connection.start().catch((err) => console.log("Meh can't start", err));
 
-onMount(() => {
-connection.start().catch((e) => console.log("On Mount Err", e));
-})
-
-	
-connection.on("send", data => {
-    console.log("Zasto zoves send?", data);
-});
-connection.on('receive', data => {
-    console.log("Podaci sa servera", data)
-    serverData = serverData ? serverData + data : data;
-})
-
-
-connection.start().catch((err) => console.log("Meh can't start", err));
-
-
-const sendMessage = () => {
-connection.invoke("SendMessage", '123', '123', msg, null)
-
-}
-
+	const sendMessage = () => {
+		connection.invoke('SendMessage', '123', '123', msg, null);
+	};
 </script>
 
 <svelte:head>
@@ -46,26 +42,29 @@ connection.invoke("SendMessage", '123', '123', msg, null)
 </svelte:head>
 
 <section>
-    <h1>Chat Tester</h1>
-        <textarea value={serverData} class='big-box' disabled/>
-        <input placeholder="Type Message" value={msg} on:change={(event) => msg = (event.currentTarget.value)}/>
-        <button on:click={sendMessage}>Send it</button>
+	<h1>Chat Tester</h1>
+	<textarea value={serverData} class="big-box" disabled />
+	<input
+		placeholder="Type Message"
+		value={msg}
+		on:change={(event) => (msg = event.currentTarget.value)}
+	/>
+	<button on:click={sendMessage}>Send it</button>
 </section>
 
 <style>
-    .big-box {
-    width: 100%;
-    min-height: 50%;
-    padding-bottom: 50px;
-    margin-bottom: 100px;
-    }
+	.big-box {
+		width: 100%;
+		min-height: 50%;
+		padding-bottom: 50px;
+		margin-bottom: 100px;
+	}
 
-
-    input {
-    width: 60%;
-    padding-bottom: 35px;
-    margin-bottom: 30px;
-    }
+	input {
+		width: 60%;
+		padding-bottom: 35px;
+		margin-bottom: 30px;
+	}
 
 	section {
 		display: flex;
@@ -79,4 +78,3 @@ connection.invoke("SendMessage", '123', '123', msg, null)
 		width: 100%;
 	}
 </style>
-
--- admin-portal/vite.config.ts
+++ admin-portal/vite.config.ts
@@ -2,6 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+	server: {
+		watch: {
+			usePolling: true
+		},
+		host: '0.0.0.0',
+		hmr: {
+			port: 3101
+		}
+	},
 	plugins: [sveltekit()]
 };
 

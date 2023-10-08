80262cf8b48750cb80842a9abf7e9c3679757ac1
Dragan <Dragan>
2023-10-08 13:28:53 +0200

    qqq

--- auth-service/src/utils/builder_helper.rs
+++ auth-service/src/utils/builder_helper.rs
@@ -10,6 +10,8 @@ pub fn res_builder(status: StatusCode, body: String) -> Result<Response<BoxBody>
     let response = Response::builder()
         .status(status)
         .header(header::CONTENT_TYPE, "application/json")
+        .header(header::ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+        .header(header::ACCESS_CONTROL_ALLOW_HEADERS, "*")
         .body(full(body))
         .unwrap();
     Ok(response)

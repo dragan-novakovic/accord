477400acd9d4243c5cff2c096f8329954667d6b2
Dragan <Dragan>
2023-11-05 12:56:49 +0100

    qqq

---
+++ admin-portal/src/routes/register/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import { useForm, validators, HintGroup, Hint, email, required } from 'svelte-use-form';
+
+	const form = useForm();
+
+	const login = async () => {
+		//console.log(form); // store(Observable) and Action
+		const x = await fetch('http://auth:3000/status');
+		console.log(x.json());
+		// const response = await fetch('http://auth:3000/register', {
+		// 	method: 'POST',
+		// 	body: JSON.stringify({ username: 'Test1', password: '123' })
+		// });
+		// const data = await response.json();
+		// console.log({ data });
+	};
+</script>
+
+<svelte:head>
+	<title>Admin Portal | Login</title>
+</svelte:head>
+
+<section>
+	<h1>Login</h1>
+</section>
+<form use:form>
+	<input type="email" name="email" use:validators={[required]} />
+	<HintGroup for="email">
+		<Hint on="required">This is a mandatory field</Hint>
+		<Hint on="email" hideWhenRequired>Email is not valid</Hint>
+	</HintGroup>
+
+	<input type="password" name="password" />
+	<Hint for="password" on="required">This is a mandatory field</Hint>
+    <div on:click={login}>helloooooo</div>
+	<button disabled={!$form.valid} on:click={login}>Login</button>
+</form>
+<pre>
+{JSON.stringify($form, null, ' ')}
+</pre>
+
+<style>
+	:global(.touched:invalid) {
+		border-color: red;
+		outline-color: red;
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+</style>

e476835300cca85c880d0321ee2b25be2a538d3d
Dragan <Dragan>
2023-11-06 22:47:19 +0100

    qqq

--- api-gateway/routes/auth.ts
+++ api-gateway/routes/auth.ts
@@ -5,7 +5,8 @@ export const AUTH__BASE_URL = "http://auth:3000";
 export const AUTH = {
   async getStatus(context: RouterContext<"/auth/status">) {
     const response = await fetch("http://auth:3000/status");
-    console.log(response.json());
+    const data = await response.json();
+    console.log(data);
     context.response.body = response.statusText;
   },
 };

9fbda4f1e12505e647c289dae67f3f817901b288
Dragan <Dragan>
2023-11-09 23:24:52 +0100

    qqq

--- chat-service/Program.cs
+++ chat-service/Program.cs
@@ -5,6 +5,7 @@ using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Authentication.Certificate;
 using Microsoft.AspNetCore.Http.Connections;
 
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
@@ -13,11 +14,11 @@ builder.Logging.AddConsole();
 IConfigurationRoot CONFIG = builder.Configuration.AddJsonFile("appsettings.json").Build();
 IConfigurationRoot CONFIG_CERT = builder.Configuration.AddJsonFile("certificate.json").Build();
 
-var certificateSettings = CONFIG_CERT.GetSection("certificateSettings");
-string certificateFileName = certificateSettings.GetValue<string>("filename");
-string certificatePassword = certificateSettings.GetValue<string>("password");
+// var certificateSettings = CONFIG_CERT.GetSection("certificateSettings");
+// string certificateFileName = certificateSettings.GetValue<string>("filename");
+// string certificatePassword = certificateSettings.GetValue<string>("password");
 
-var cerificate = new X509Certificate2(certificateFileName, certificatePassword);
+// var cerificate = new X509Certificate2(certificateFileName, certificatePassword);
 
 Console.WriteLine("Using" + CONFIG.GetValue<string>("useDB"));
 switch (CONFIG.GetValue<string>("useDB"))
@@ -77,7 +78,7 @@ builder.Services.AddControllers();
 builder.Services.AddSignalR(hubOptions => { hubOptions.HandshakeTimeout = TimeSpan.FromSeconds(15); hubOptions.KeepAliveInterval = TimeSpan.FromSeconds(15); hubOptions.EnableDetailedErrors = true; });
 builder.Services.AddMemoryCache();
 builder.Services.AddScoped<IMessageService, MessageService>(sp => new MessageService(sp.GetService<IMongoClient>()));
-builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
+//builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme).AddCertificate();
 
 
 WebApplication app = builder.Build();
